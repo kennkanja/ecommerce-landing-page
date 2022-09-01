08c9ec7fd122e6ba4a2eb7b589af037a6a0ad9ba
kenn <44947639+kennkanja@users.noreply.github.com>
2022-09-01 12:04:04 +0300

    ecommerce landing page
    understanding how to display and layout a page usig html and css

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+window.onscroll = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+const sr= ScrollReveal({
+    distance: '30px',
+    duration: 2600,
+    reset: true
+})
+sr.reveal('.home-text', {delay:280, origin:'bottom'})
+
+sr.reveal('.featured,.cta,.new,.brand,.contact', {delay:280, origin:'bottom'})
